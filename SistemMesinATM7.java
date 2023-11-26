d81e3c5a297b70290c7430767dbb813ddce551ce
qusnul_diah <qusnuldiah25@gmail.com>
2023-11-26 22:16:42 +0700

    pengaduan masalah dan transfer

--- SistemMesinATM7.java
+++ SistemMesinATM7.java
@@ -6,6 +6,7 @@ public class SistemMesinATM7 {
     public static class Account {  
         int saldoAwal; 
         int pin, nomorRekening;
+        String masalahLaporan;
 
         public Account(int pin, int nomorRekening, int saldoAwal){
             this.pin = pin;
@@ -13,8 +14,7 @@ public class SistemMesinATM7 {
             this.saldoAwal = saldoAwal;
         }
         public void reportProblem(int tanggal, String masalah) {
-            // Logic to report the problem
-            System.out.println("Problem reported for account " + nomorRekening + " on " + tanggal + ": " + masalah);
+            masalahLaporan = masalah;
         }
         }
 
@@ -159,29 +159,36 @@ public class SistemMesinATM7 {
                         System.out.println("Setoran berhasil. Saldo Anda sekarang: " + currentAccount.saldoAwal);
                         break;
                     case 4:
+                        kodeTransaksi = random.nextInt(1000000);
                         System.out.print("Masukkan nomor rekening tujuan: ");
                         sc.nextDouble();
                         System.out.print("Nominal transfer: ");
                         double transfer = sc.nextDouble();
-                        if (currentAccount.saldoAwal <= transfer){
+                        if (transfer <= currentAccount.saldoAwal){
+                        currentAccount.saldoAwal -= transfer;
+                        System.out.println("Kode : "+ kodeTransaksi);
                         System.out.println("Transfer berhasil. Saldo Anda sekarang: " + currentAccount.saldoAwal);
                         } else {
                             System.out.println("Saldo Tidak mencukupi");
                         }
                         break;
                     case 5:
-                        System.out.println("Pengaduan Masalah  ");
+                        System.out.println("===== Pengaduan Masalah =====");
                         
-                        System.out.println("Masukkan No Rekening :");
+                        System.out.print("Masukkan No Rekening :");
                         nomorRekening = sc.nextInt();
-                        System.out.println("Masukkan Tanggal (dd/mm/yy)");
-                        int tanggal = sc.nextInt();
+                        System.out.print("Masukkan Tanggal (dd/mm/yy): ");
+                        String tanggalInput = sc.next();
                         sc.nextLine();
+                        String[] parts = tanggalInput.split("/");
+                        int tanggal = Integer.parseInt(parts[0]);
+                        int bulan = Integer.parseInt(parts[1]);
+                        int tahun = Integer.parseInt(parts[2]);
                         System.out.println("Permasalahan yang Anda alami :");
                         String masalah = sc.nextLine();
-                        System.out.println("Masukkan PIN: ");
+                        System.out.print("Masukkan PIN: ");
                         int inputPin = sc.nextInt();
-                        sc.nextLine(); // Consume the newline character
+                        sc.nextLine();
                         if (inputPin != currentAccount.pin) {
                          System.out.println("PIN tidak sesuai. Pengaduan masalah dibatalkan.");
                         }
@@ -194,6 +201,12 @@ public class SistemMesinATM7 {
                         }
                         if (selectedAccount != null) {
                             selectedAccount.reportProblem(tanggal, masalah);
+                            System.out.println("===== Data Akun Terkait =====");
+                            System.out.println("Nomor Rekening: " + selectedAccount.nomorRekening);
+                            System.out.println("Saldo Awal: " + selectedAccount.saldoAwal);
+                            System.out.println("Tanggal Laporan: " + String.format("%02d/%02d/%02d", tanggal, bulan, tahun));
+                            System.out.println("Masalah Dilaporkan: " + selectedAccount.masalahLaporan);
+                            System.out.println("Terima kasih atas laporan masalah Anda. Masalah akan segera diproses dan ditangani.");
                         } else {
                             System.out.println("Account not found");
                         }
